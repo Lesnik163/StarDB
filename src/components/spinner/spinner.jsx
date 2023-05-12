50cc1e82743b28532f0908ebc7fe836c509f3669
Dmitrii Liubich <airvaffeforce@yandex.ru>
2023-05-12 17:08:28 +0300

    random-planet was rendered

--- src/components/spinner/spinner.jsx
+++ src/components/spinner/spinner.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import './spinner.css'
 function Spinner() {
   return (
-    <div class="loadingio-spinner-double-ring-g2hzejtoz3w">
-      <div class="ldio-179d57ig63k">
+    <div className="loadingio-spinner-double-ring-g2hzejtoz3w">
+      <div className="ldio-179d57ig63k">
         <div></div>
         <div></div>
         <div><div>
