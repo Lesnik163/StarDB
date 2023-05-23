f64de7c0d845f44dc9846728891b4db8bb3562ea
Dmitrii Liubich <airvaffeforce@yandex.ru>
2023-05-23 09:52:40 +0300

    People,Starship,Planet pages was added

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -6,7 +6,7 @@ const Header = ({onServiceChange}) => {
   return (
     <div className="header d-flex">
       <h3>
-        <a href="#">
+        <a href="xxx">
           StarDB
         </a>
       </h3>
---
+++ src/components/pages/index.js
@@ -0,0 +1,5 @@
+import PeoplePage from "./people-pages";
+import PlanetPage from "./planet-pages";
+import StarshipPage from "./starship-pages";
+
+export  {PeoplePage, PlanetPage, StarshipPage} 
